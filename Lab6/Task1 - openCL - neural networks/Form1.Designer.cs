fd9308cfdf0df59d237674751a73df460c421966
Krzysztof Bojakowski <krzysztof0bojakowski@gmail.com>
2015-06-06 00:38:25 +0200

    random, neurals

---
+++ Lab6/Task1 - openCL - neural networks/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Task1___openCL___neural_networks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(231, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Print result";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(135, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "RESULT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 178);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Lab6/Task1 - openCL - neural networks/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1___openCL___neural_networks
+{
+    public partial class Form1 : Form
+    {
+        double[,] array;
+        int height, width;
+        Bitmap image;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image File|*.png";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                image = new Bitmap(openFileDialog1.OpenFile());
+                width = image.Width;
+                height = image.Height;
+                array = new double[width, height];
+
+                int pixel;
+                for (int i = 0; i < width; i++)
+                    for (int j = 0; j < height; j++)
+                    {
+                        pixel = image.GetPixel(i, j).B;
+                        pixel += image.GetPixel(i, j).G;
+                        pixel += image.GetPixel(i, j).R;
+                        pixel /= 3;
+                        if (pixel > 200)
+                            array[i, j] = 0;
+                        else if (pixel > 150 && pixel < 200)
+                            array[i, j] = 0.25f;
+                        else if (pixel > 100 && pixel < 150)
+                            array[i, j] = 0.5f;
+                        else if (pixel > 50 && pixel < 100)
+                            array[i, j] = 0.75f;
+                        else array[i, j] = 1; 
+                    }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double sum = 0;
+            Random rand = new Random();
+
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    sum += array[i, j] * rand.NextDouble() * ( 100 - 0 ) + 0; // ( Max - Min ) + Min
+            double euler = 2.71;
+            double output = 1 / (1 + Math.Pow(euler, -1 * sum));
+            label1.Text = output.ToString();
+        }  
+    }
+}
